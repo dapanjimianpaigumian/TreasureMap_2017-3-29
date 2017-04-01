c44b0e9f065c5e70caa9de7c18d9486e26e11e28
dapanjimianpaigumian <zhao_xin_peng@hotmail.com>
2017-04-01 12:38:46 +0800

    初步集成百度地图 SDK

--- app/src/main/java/com/yulu/zhaoxinpeng/mytreasuremap/TreasureApplication.java
+++ app/src/main/java/com/yulu/zhaoxinpeng/mytreasuremap/TreasureApplication.java
@@ -3,6 +3,7 @@ package com.yulu.zhaoxinpeng.mytreasuremap;
 import android.app.Application;
 
 
+import com.baidu.mapapi.SDKInitializer;
 import com.yulu.zhaoxinpeng.mytreasuremap.activity.user.UserPrefs;
 
 /**
@@ -17,6 +18,11 @@ public class TreasureApplication extends Application {
 
         // 用户仓库的初始化
         UserPrefs.init(getApplicationContext());
+
+        // 百度地图SDK的初始化
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
 
     }
 }
